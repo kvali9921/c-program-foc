ea7d3548d6ee13ca3211af3a1a80d101ab4643ad
kvali9921 <124122353+kvali9921@users.noreply.github.com>
2023-02-06 09:21:07 +0530

    Create jgcudc.c

---
+++ jgcudc.c
@@ -0,0 +1,24 @@
+id++ id--
+              variable post-increment and post-decrement
+       ++id --id
+              variable pre-increment and pre-decrement
+       - +    unary minus and plus
+       ! ~    logical and bitwise negation
+       **     exponentiation
+       * / %  multiplication, division, remainder
+       + -    addition, subtraction
+       << >>  left and right bitwise shifts
+       <= >= < >
+              comparison
+       == !=  equality and inequality
+       &      bitwise AND
+       ^      bitwise exclusive OR
+       |      bitwise OR
+       &&     logical AND
+       ||     logical OR
+       expr?expr:expr
+              conditional operator
+       = *= /= %= += -= <<= >>= &= ^= |=
+              assignment
+       expr1 , expr2
+              comma
